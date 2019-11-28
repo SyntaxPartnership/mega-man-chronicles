a4161303fade31b826916c1ebbab1a400fde2db7
9BitStrider <9bitstrider@gmail.com>
2019-11-28 13:15:09 -0600

    Laser Gabyoall Test

--- scripts/enemies/laser_gabyoall.gd
+++ scripts/enemies/laser_gabyoall.gd
@@ -6,12 +6,11 @@ extends KinematicBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for set in (5):
-		print(set)
-		if !is_on_ceiling():
-			$box_top.position.y += 16
-			$sprite_top.position.y += 16
-
+	for set in (10):
+		var overlap = $hit_box_top.get_overlapping_bodies()
+		$hit_box_top.position.y -= 16
+		$sprite_top.position.y -= 16
+		print(overlap)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
